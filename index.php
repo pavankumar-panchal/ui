8561e46d08d825d2f161c248df3f9ee01dab02cf
pavankumar <panchalpavan800@gmail.com>
2023-07-31 13:55:48 +0530

    update

--- index.php
+++ index.php
@@ -1,68 +1,85 @@
-<?php
-include("navigation/nav.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="card mb-4 mt-4">
-  <div class="card-body">
-    <div class="d-flex justify-content-between">
-      <div>
-        <h4 class="card-title mb-0"> Chart on count of Calls and emails</h4>
-        <div class="small text-medium-emphasis">January - July 2022</div>
-      </div>
-      <div class="btn-toolbar d-none d-md-block" role="toolbar" aria-label="Toolbar with buttons">
-        <div class="btn-group btn-group-toggle mx-3" data-coreui-toggle="buttons">
-          <input class="btn-check" id="option1" type="radio" name="options" autocomplete="off">
-          <label class="btn btn-outline-secondary"> Day</label>
-          <input class="btn-check" id="option2" type="radio" name="options" autocomplete="off" checked="">
-          <label class="btn btn-outline-secondary active"> Month</label>
-          <input class="btn-check" id="option3" type="radio" name="options" autocomplete="off">
-          <label class="btn btn-outline-secondary"> Year</label>
-        </div>
-        <button class="btn btn-primary" type="button">
-          <svg class="icon">
-            <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-cloud-download"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-    <div class="c-chart-wrapper" style="height:300px;margin-top:40px;">
-      <canvas class="chart" id="main-chart" height="300"></canvas>
-    </div>
-  </div>
-</div>
-<!-- /.row-->
-<div class="row">
-  <div class="col-md-12">
-    <div class="card mb-4">
-      <div class="card-header"> Attendance Summary of this Month</div>
-      <div class="card-body">
-        <div class="row">
-          <!-- Attendence  -->
-        </div>
-        <div class="container mt-4">
-          <p>
-            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
-              aria-expanded="false" aria-controls="collapseExample" id="toggleButton">
-              Team Chart
-            </button>
-          </p>
-          <div class="collapse" id="collapseExample">
-            <div class="card card-body">
-              Team chart.
-            </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Login System</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <!-- Custom styles for login page -->
+  <style>
+    body {
+      background-color: #f8f9fa;
+    }
+    .login-container {
+      max-width: 450px;
+      margin: 0 auto;
+      padding: 40px;
+      border-radius: 5px;
+      box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+      background-color: #ffffff;
+    }
+
+    .login-logo {
+      text-align: center;
+      margin-bottom: 20px;
+    }
+
+    .login-logo img {
+      width: 200px;
+      height: auto;
+    }
+
+    .btn-primary {
+      background-color: #007bff;
+      border-color: #007bff;
+    }
+
+    .btn-primary:hover {
+      background-color: #0056b3;
+      border-color: #0056b3;
+    }
+
+    .btn-link {
+      color: #007bff;
+    }
+
+    .btn-link:hover {
+      color: #0056b3;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <div class="login-container">
+          <div class="login-logo">
+            <img src="assets/img/avatars/ssmwhite.png" alt="Logo" class="img-fluid">
           </div>
+          <form>
+            <div class="mb-3">
+              <label for="username" class="form-label">User Name:</label>
+              <input type="text" class="form-control" id="username" placeholder="Enter your username">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password:</label>
+              <input type="password" class="form-control" id="password" placeholder="Enter your password">
+            </div>
+            <div class="d-grid gap-2">
+              <button type="submit" class="btn btn-primary">Login</button>
+              <button type="reset" class="btn btn-secondary">Clear</button>
+            </div>
+          </form>
         </div>
       </div>
-      <!-- stops here -->
-    </div class="col-md-12">
-    <!-- <div id="wrapper" class="table-container"></div> -->
+    </div>
   </div>
-</div>
-</div>
 
-<?php
+  <!-- Bootstrap JS (Make sure to include jQuery and Popper.js before this) -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 
-include("navigation/footer.php");
-
-
-?>
+</html>
